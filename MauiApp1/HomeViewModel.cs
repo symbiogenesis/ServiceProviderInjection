26821da4a5e94e4dac36a444775b1306b0bd1ceb
Edward Miller <symbiogenesis@outlook.com>
2023-02-04 01:40:16 -0600

    Add project files.

---
+++ MauiApp1/HomeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    public class HomeViewModel : BindableObject
+    {
+        public HomeViewModel()
+        {
+
+        }
+
+        public string Title { get; set; } = "Hello Ram";
+    }
+}
